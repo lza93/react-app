ddd5669c2cfaf49fe327a1c9d94afaca571e93b7
dejongnj <dejongnj@gmail.com>
2016-12-30 15:08:35 -0600

    minor styling changes

--- src/client/jsx/reducers/userReducer.js
+++ src/client/jsx/reducers/userReducer.js
@@ -4,9 +4,8 @@ import initialState from './userInitialState';
 export default function (state = initialState, action) {
   switch (action.type) {
     case SET_USER:
-      console.log("SET_USER IN REDUCER WAS CALLED");
       return action.user;
     default:
-      return state;   
+      return state;
   }
-};
+}
