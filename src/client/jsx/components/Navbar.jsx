816e0125286429d3f5ac30af819b76a6ecce492c
dejongnj <dejongnj@gmail.com>
2017-01-01 16:14:42 -0600

    navbar rendering tests

--- src/client/jsx/components/Navbar.jsx
+++ src/client/jsx/components/Navbar.jsx
@@ -15,7 +15,7 @@ const renderLoginLink = () => (
 
 const renderLogoutButton = logout => (
   <form className="navbar-form navbar-left">
-    <button className="btn btn-default" onClick={(e) => { logout(e); }}>
+    <button id="logout-navbar-button" className="btn btn-default" onClick={(e) => { logout(e); }}>
       logout
     </button>
   </form>
