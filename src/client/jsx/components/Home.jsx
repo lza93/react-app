19574b718f2241d62783b1e85baadabdb92c1b47
dejongnj <dejongnj@gmail.com>
2017-01-02 10:45:41 -0600

    fixed home page styling

--- src/client/jsx/components/Home.jsx
+++ src/client/jsx/components/Home.jsx
@@ -8,8 +8,12 @@ Starting point for building a react app with signup/login functionality, license
 
 const explanatoryParagraphs = `Sometimes the most difficult part of a project is getting started. And then having to reinvent the wheel. Here is a great starter app for react-developers from which anyone can build. It offers an easy way for anyone to get started with programming in react-redux, and then tying in the built in login/signup functionality.
 The reason I built this was to take the grunt work out of setting up a lot of boilerplate code, and instead decided to go with some sensible defaults. I also endeavored to set up a testing environment with some basic tests off of which anyone can build. This makes test-driven-development easier to implement right off the bat, without struggling to set up the environments yourself.
-Please feel free to add your suggestions, bearing in mind the purpose of this app, and/or fixes to bugs you experience.`;
+Please feel free to add your suggestions (bearing in mind the purpose of this app) and/or fixes to bugs you experience.`;
 
+const horizontalStyle = {
+  minHeight: '25vh',
+  lineHeight: '25vh',
+};
 
 const Home = () => (
   <div id="home">
@@ -23,7 +27,7 @@ const Home = () => (
       </div>
     </div>
     <div id="home-info">
-      {renderParagraphs(explanatoryParagraphs)}
+      {renderParagraphs(explanatoryParagraphs, horizontalStyle)}
     </div>
   </div>
 );
